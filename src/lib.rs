c0916c95fc86842d2f5ef93e7cedba3447c684ce
Mohammad El Masri <148862917+mohammadmasry@users.noreply.github.com>
2024-03-20 23:06:15 +0100

    Update lib.rs
    fanction that calculate instead of iteration

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,14 @@
 pub fn square_of_sum(n: u32) -> u32 {
-    let sum = (1..=n).sum::<u32>();
+    let sum = n * (n + 1) / 2; // Formula for sum of arithmetic series
     sum * sum
 }
 
 pub fn sum_of_squares(n: u32) -> u32 {
-    (1..=n).map(|x| x * x).sum::<u32>()
+    n * (n + 1) * (2 * n + 1) / 6; // Formula for sum of squares
 }
 
 pub fn difference(n: u32) -> u32 {
-    square_of_sum(n) - sum_of_squares(n)
+    let square_of_sum = square_of_sum(n);
+    let sum_of_squares = sum_of_squares(n);
+    square_of_sum - sum_of_squares
 }
